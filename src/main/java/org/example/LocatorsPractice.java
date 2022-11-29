c4454dd49debd3905c3feccc7e8446304f7b4c6c
dpopesco <diana.popesco17@gmail.com>
2022-11-29 10:38:52 +0200

    Renamed

--- src/main/java/org/example/LocatorsPractice.java
+++ src/main/java/org/example/LocatorsPractice.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.net.MalformedURLException;
 import java.time.Duration;
 
-public class SeleniumProject {
+public class LocatorsPractice {
     public static void main(String[] args) throws MalformedURLException {
         WebDriver driver = new ChromeDriver();
         //WebDriver driver = new FirefoxDriver();
